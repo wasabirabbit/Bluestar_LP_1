eb81801f929b6712f94129ebfd55dddcb1c3a222
joeymilanes <jmilanes@wasabirabbit.com>
2015-01-23 14:37:04 -0500

    Auto-generated commit by Joseph Milanes

--- tasks/options/gh-pages.js
+++ tasks/options/gh-pages.js
@@ -2,7 +2,7 @@ module.exports = {
 'gh-pages': {
     options: {
       branch: 'master',
-      message: 'Auto-generated commit'
+      message: 'Auto-generated commit by Joseph Milanes'
     },
     src: ['**']
   }
