e2e976a040fd61deae467d2634f444a2345a1dea
joeymilanes <jmilanes@wasabirabbit.com>
2015-01-27 12:05:57 -0500

    Updates

--- tasks/options/cssmin.js
+++ tasks/options/cssmin.js
@@ -1,7 +1,7 @@
 module.exports = {
 	target: {
     files: {
-      'css/output.min.css': ['css/BSV_Main.css','css/helpers/*.css', 'css/plugin/*.css'],
+      'output.min.css': ['css/BSV_Main.css','css/helpers/*.css', 'css/plugin/*.css']
     }
   }
 }
